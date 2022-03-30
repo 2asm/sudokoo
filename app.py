320965a6f865217ca0dd849174fd82201eb191d4
dbugd <0077dk0077@gmail.com>
2022-03-30 08:22:01 +0530

    force https

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, redirect, request
 from sudoku import generate_sudoku
 app = Flask(__name__)
-  
+
+@app.before_request
+def before_request():
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
+
 @app.route('/',methods = ['GET'])
 def index():
     return render_template("index.html")
